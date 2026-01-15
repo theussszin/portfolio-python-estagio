5e5517bc93e13761b3a3b61fa51a4006c2af5378
theussszin <mh763559@gmail.com>
2026-01-15 13:15:43 -0300

    Add simple calculator functionality in Python

---
+++ calculadora/calculadora.py
@@ -0,0 +1,23 @@
+print("Calculadora Simples")
+
+num1 = float(input("Digite o primeiro número: "))
+num2 = float(input("Digite o segundo número: "))
+
+print("Escolha a operação:")
+print("1 - Soma")
+print("2 - Subtração")
+print("3 - Multiplicação")
+print("4 - Divisão")
+
+opcao = input("Opção: ")
+
+if opcao == "1":
+    print("Resultado:", num1 + num2)
+elif opcao == "2":
+    print("Resultado:", num1 - num2)
+elif opcao == "3":
+    print("Resultado:", num1 * num2)
+elif opcao == "4":
+    print("Resultado:", num1 / num2)
+else:
+    print("Opção inválida")
